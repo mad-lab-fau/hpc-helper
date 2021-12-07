fb69690d39ccd0e14a55e5653ca62de11682c359
Robert Richer <robert.richer@fau.de>
2021-12-07 13:38:13 +0100

    cleaned up repo

--- hpc_helper/_exceptions.py
+++
@@ -1,7 +0,0 @@
-"""A set of custom exceptions."""
-
-__all__ = ["FileExtensionError"]
-
-
-class FileExtensionError(Exception):
-    """An error indicating that the file name has the wrong file extension."""
